b1513bf1380f4113a1005822b10ff593477e3fff
JanThorwarth <thorwarth.jan@web.de>
2024-09-05 11:00:11 +0200

    added card description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartScreenComponent } from "./start-screen/start-screen.component";
+import { GameComponent } from './game/game.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { GameInfoComponent } from './game-info/game-info.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StartScreenComponent],
+  imports: [RouterOutlet, StartScreenComponent, GameComponent, MatButtonModule, MatDividerModule, MatIconModule, GameInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,25 +1,76 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
+import { MatButtonModule } from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from "../game-info/game-info.component";
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent, PlayerComponent, MatButtonModule, MatDividerModule, MatIconModule, GameInfoComponent, GameInfoComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false
+  currentCard: string | undefined = '';
+  game!: Game;
+
+  constructor(public dialog: MatDialog ) {}
+
+  ngOnInit(): void {
+      this.newGame();
+  }
+  
 
 
+
+  newGame() {
+      this.game = new Game();
+      console.log(this.game);
+      
+  }
 
   takeCard() {
-    this.pickCardAnimation = true
+    if(!this.pickCardAnimation) {
+    this.currentCard = this.game.stack.pop();
+    this.pickCardAnimation = true;
+   
+  
+
+    setTimeout(() => {
+      this.game.playedCards.push(this.currentCard ?? "")
+      this.pickCardAnimation = false
+    }, 1000);
+    
+  }
 
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name)
+      
+    });
 
 
 
+  }
 
 }
